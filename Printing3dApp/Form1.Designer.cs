fd6f3ebabfc5af8d83a57cc35e96ad8896a5af5b
Nnadozie Eneremadu <neneremadu@yahoo.com>
2022-08-30 16:00:50 +0100

    Add project files.

---
+++ Printing3dApp/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Printing3dApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbProjectTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbOwnerName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbBuildHeight = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpDateCreated = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbMaterial = new System.Windows.Forms.ComboBox();
+            this.cbProcess = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rtbComments = new System.Windows.Forms.RichTextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(215, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add New Project";
+            // 
+            // tbProjectTitle
+            // 
+            this.tbProjectTitle.Location = new System.Drawing.Point(12, 113);
+            this.tbProjectTitle.Name = "tbProjectTitle";
+            this.tbProjectTitle.Size = new System.Drawing.Size(205, 20);
+            this.tbProjectTitle.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Project title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(433, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Owner name";
+            // 
+            // tbOwnerName
+            // 
+            this.tbOwnerName.Location = new System.Drawing.Point(433, 113);
+            this.tbOwnerName.Name = "tbOwnerName";
+            this.tbOwnerName.Size = new System.Drawing.Size(205, 20);
+            this.tbOwnerName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Build height (Millimeters)";
+            // 
+            // tbBuildHeight
+            // 
+            this.tbBuildHeight.Location = new System.Drawing.Point(12, 192);
+            this.tbBuildHeight.Name = "tbBuildHeight";
+            this.tbBuildHeight.Size = new System.Drawing.Size(205, 20);
+            this.tbBuildHeight.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(433, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Date created:";
+            // 
+            // dtpDateCreated
+            // 
+            this.dtpDateCreated.Enabled = false;
+            this.dtpDateCreated.Location = new System.Drawing.Point(433, 189);
+            this.dtpDateCreated.MinDate = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
+            this.dtpDateCreated.Name = "dtpDateCreated";
+            this.dtpDateCreated.Size = new System.Drawing.Size(205, 20);
+            this.dtpDateCreated.TabIndex = 9;
+            this.dtpDateCreated.Value = new System.DateTime(2022, 8, 30, 14, 53, 58, 0);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Material";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(433, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Process";
+            // 
+            // cbMaterial
+            // 
+            this.cbMaterial.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMaterial.FormattingEnabled = true;
+            this.cbMaterial.Items.AddRange(new object[] {
+            "Aluminium, Grade 2024, Thickness 1.6mm",
+            "Titanium, Grade 5055, Thickness 2mm",
+            "Steel, Grade ER 60, Thickness 4mm"});
+            this.cbMaterial.Location = new System.Drawing.Point(12, 282);
+            this.cbMaterial.Name = "cbMaterial";
+            this.cbMaterial.Size = new System.Drawing.Size(205, 21);
+            this.cbMaterial.TabIndex = 12;
+            // 
+            // cbProcess
+            // 
+            this.cbProcess.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbProcess.FormattingEnabled = true;
+            this.cbProcess.Items.AddRange(new object[] {
+            "Plasma",
+            "MIG"});
+            this.cbProcess.Location = new System.Drawing.Point(436, 282);
+            this.cbProcess.Name = "cbProcess";
+            this.cbProcess.Size = new System.Drawing.Size(205, 21);
+            this.cbProcess.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(9, 422);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Comments:";
+            // 
+            // rtbComments
+            // 
+            this.rtbComments.Location = new System.Drawing.Point(12, 454);
+            this.rtbComments.Name = "rtbComments";
+            this.rtbComments.Size = new System.Drawing.Size(626, 137);
+            this.rtbComments.TabIndex = 15;
+            this.rtbComments.Text = "";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSubmit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSubmit.Location = new System.Drawing.Point(543, 610);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(95, 40);
+            this.btnSubmit.TabIndex = 16;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCancel.Location = new System.Drawing.Point(433, 610);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(95, 40);
+            this.btnCancel.TabIndex = 17;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Active",
+            "Closed"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 353);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(107, 21);
+            this.comboBox1.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(9, 333);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 17);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(666, 662);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.rtbComments);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cbProcess);
+            this.Controls.Add(this.cbMaterial);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtpDateCreated);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbBuildHeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbOwnerName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbProjectTitle);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbProjectTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbOwnerName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbBuildHeight;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpDateCreated;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbMaterial;
+        private System.Windows.Forms.ComboBox cbProcess;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RichTextBox rtbComments;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Printing3dApp/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace Printing3dApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, System.EventArgs e)
+        {
+
+            try
+            {
+                string projectTitle = tbProjectTitle.Text;
+                string ownerName = tbOwnerName.Text;
+                string buildHeight = tbBuildHeight.Text;
+                var dateCreated = dtpDateCreated.Value;
+                var material = cbMaterial.Text;
+                var process = cbProcess.Text;
+                string comments = rtbComments.Text;
+                var isValid = true;
+
+
+                if (string.IsNullOrWhiteSpace(projectTitle))
+                {
+                    isValid = false;
+                    MessageBox.Show("Poject Title is Required");
+                }
+
+                if (string.IsNullOrWhiteSpace(ownerName))
+                {
+                    isValid = false;
+                    MessageBox.Show("Owner name is Required");
+                }
+                if (material == null)
+                {
+                    isValid = false;
+                    MessageBox.Show("Please select a Material value!");
+                }
+
+                if (process == null)
+                {
+                    isValid = false;
+                    MessageBox.Show("Please select a Material value!");
+                }
+
+
+                if (isValid)
+                {
+                    MessageBox.Show($"Project: {projectTitle}\n\r" +
+                    $"created by: {ownerName} " +
+                    $" is successful on {dateCreated}");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+                //throw;
+            }
+
+
+        }
+    }
+}
